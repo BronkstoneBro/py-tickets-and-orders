800bb82106ea66b9047ab03bc55cfe079ca55188
BronkstoneBro <atlantwindow@gmail.com>
2025-03-27 09:24:06 +0200

    fix mistakes in order

--- services/order.py
+++ services/order.py
@@ -7,13 +7,12 @@ from db.models import Order, Ticket
 
 User = get_user_model()
 
-
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> Order:
-    with transaction.atomic():
         user = User.objects.get(username=username)
         order = Order.objects.create(user=user)
 
