7f06fba241a9fca99057688064a964a59f71c44f
BronkstoneBro <atlantwindow@gmail.com>
2025-03-27 09:40:24 +0200

    fix mistakes in order

--- services/order.py
+++ services/order.py
@@ -7,28 +7,28 @@ from db.models import Order, Ticket
 
 User = get_user_model()
 
+
 @transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
-        date: str = None
-) -> Order:
-        user = User.objects.get(username=username)
-        order = Order.objects.create(user=user)
+        date: str = None) -> Order:
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
 
-        if date:
-            order.created_at = date
-            order.save()
+    if date:
+        order.created_at = date
+        order.save()
 
-        for ticket in tickets:
-            Ticket.objects.create(
-                row=ticket["row"],
-                seat=ticket["seat"],
-                movie_session_id=ticket["movie_session"],
-                order=order
-            )
+    for ticket in tickets:
+        Ticket.objects.create(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session"],
+            order=order
+        )
 
-        return order
+    return order
 
 
 def get_orders(username: str = None) -> QuerySet:
